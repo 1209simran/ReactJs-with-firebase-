40c6d70f94faa64182d6fa3b8f0eeacacc397a10
1209simran <37840881+1209simran@users.noreply.github.com>
2019-05-18 23:06:08 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyA6HY-IBSaV2EL3JgpZseZj7WxPGO6YQkY",
-  authDomain: "contact-93671.firebaseapp.com",
-  databaseURL: "https://contact-93671.firebaseio.com",
-  projectId: "contact-93671",
-  storageBucket: "contact-93671.appspot.com",
-  messagingSenderId: "475485152345",
-  appId: "1:475485152345:web:00651d96e040c390"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_NAME.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_NAME.firebaseio.com",
+  projectId: "YOUR_PROJECT_NAME",
+  storageBucket: "YOUR_PROJECT_NAME.appspot.com",
+  messagingSenderId: "YOUR_MESSAGINGSENDERID",
+  appId: "YOUR APPID"
 };
 
 const firebaseConf = firebase.initializeApp(config);
